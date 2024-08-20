07420d0d761e904b0e6d1cd11ef5234175ea2894
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-08-20 14:51:09 +0200

    login, log ud og skift password funktioner er tilføjet til login siden.

--- Bagtanker/src/Components/Customers/Login/Login.jsx
+++ Bagtanker/src/Components/Customers/Login/Login.jsx
@@ -11,6 +11,13 @@ export const Login = () => {
 	const { supabase } = useSupabase();
 	const { loginData, setLoginData } = useAuth();
 
+	const clearMessages = () => {
+		setTimeout(() => {
+			setErrorMessage(""); // Tømmer errorMessage efter 5 sekunder
+			setSuccessMessage(""); // Tømmer successMessage efter 5 sekunder
+		}, 5000);
+	};
+
 	const {
 		register,
 		handleSubmit,
@@ -31,6 +38,15 @@ export const Login = () => {
 		// Hvis der opstår en fejl under login, logges fejlen til konsollen
 		if (error) {
 			console.error("Error logging in:", error);
+
+			//ændre fejlbeskeden hvis den indeholder"Invalid login credentials" til at være en meddelse på dansk
+			if (error.message.includes("Invalid login credentials")) {
+				setErrorMessage("Brugernavn eller password er ikke korrekt");
+			} else {
+				setErrorMessage(error.message);
+			}
+
+			clearMessages();
 		} else {
 			// Hvis login er succesfuldt, gemmes autentificeringstoken i sessionStorage og loginData opdateres med brugerdata
 			sessionStorage.setItem("supabase.auth.token", JSON.stringify(data));
@@ -58,8 +74,7 @@ export const Login = () => {
 		}
 
 		try {
-			setErrorMessage(""); // Sletter tidligere error beksed
-			setSuccessMessage(""); // Sletter tidligere success beksed
+			clearMessages();
 			const { user, error } = await supabase.auth.updateUser({
 				password: newPassword,
 			});
@@ -67,15 +82,18 @@ export const Login = () => {
 			if (error) {
 				setErrorMessage(error.message);
 				console.error("Error changing password:", error);
+				clearMessages();
 			} else {
 				//hvis passwordskiftet lykkedes sættes der et en succes besked og beskeden logges i konsolen med brugerens information.
 
 				setSuccessMessage("Password changed successfully");
+				clearMessages();
 				console.log("Password changed successfully for user:", user);
 			}
 			// Hvis der opstår en fejl i try-blokken, vises en fejlbesked og fejlen logges i konsollen		} catch (error) {
 		} catch (error) {
 			setErrorMessage(error.message); // Set error message
+			clearMessages();
 			console.error("Error changing password:", error.message);
 		}
 	};
@@ -114,11 +132,15 @@ export const Login = () => {
 									This field is required
 								</span>
 							)}
+							{errorMessage && (
+								<span className={globalStyle.errorMessage}>{errorMessage}</span>
+							)}
 							<div className={style.buttonContainer}>
 								<button className={globalStyle.styledButton} type="submit">
 									Login
 								</button>
 							</div>
+
 							<div className={style.link}>
 								{/* sender brugeren videre til en ny side  */}
 								<Link to="/login/createUser">Opret mig som bruger</Link>
@@ -128,80 +150,108 @@ export const Login = () => {
 				</PageWrapper>
 			) : (
 				<PageWrapper title="Min side">
-					<h2>Mine kommentare</h2>
-					<table className={style.commentsTable}>
-						<thead>
-							<tr>
-								<th>Title</th>
-								<th>Dato</th>
-								<th>Handling</th>
-							</tr>
-						</thead>
+					<div className={style.loginWrapper}>
+						<h2 className={style.subtitle}>Mine kommentare</h2>
+						{/* //skal ændres så data kommer fra api, når brugeren har skrevet en kommentar */}
+						<table className={style.commentsTable}>
+							<thead>
+								<tr>
+									<th>Title</th>
+									<th>Dato</th>
+									<th>Handling</th>
+									<th></th>
+								</tr>
+							</thead>
 
-						<tbody>
-							<tr>
-								<th>fantastisk oplelevse</th>
-								<th>fantastisk oplelevse</th>
-								<th>fantastisk oplelevse</th>
-							</tr>
-						</tbody>
-					</table>
+							<tbody>
+								<tr>
+									<td>fantastisk oplelevse</td>
+									<td>8. august 2024</td>
+									<td className={style.edit}>rediger</td>
+									<td className={style.delete}>slet</td>
+								</tr>
+								<tr>
+									<td>fantastisk oplelevse</td>
+									<td>8. august 2024</td>
+									<td className={style.edit}>rediger</td>
+									<td className={style.delete}>slet</td>
+								</tr>
+								<tr>
+									<td>fantastisk oplelevse</td>
+									<td>8. august 2024</td>
+									<td className={style.edit}>rediger</td>
+									<td className={style.delete}>slet</td>
+								</tr>
+								<tr>
+									<td>fantastisk oplelevse</td>
+									<td>8. august 2024</td>
+									<td className={style.edit}>rediger</td>
+									<td className={style.delete}>slet</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+
+					{/* ekstra funktioner som jeg mener er nyttige på denne side når en bruger er logget ind */}
+					<div className={style.buttonContainer}>
+						{/* Denne knap vil logge brugeren ud */}
+
+						<button onClick={handleLogout} className={globalStyle.styledButton}>
+							Log ud
+						</button>
+					</div>
+					<div className={style.passwordWrapper}>
+						<h2 className={style.subtitle}>Skift Password</h2>
+						{/* Når der trykkes på knappen (password) kaldes handleChangePassword funktionen */}
+						<form
+							className={style.form}
+							onSubmit={handleSubmit(handleChangePassword)}>
+							<input
+								className={globalStyle.input}
+								type="password"
+								placeholder="Nyt password"
+								{...register("newPassword", { required: true })}
+							/>
+							{errors.newPassword && (
+								<span className={globalStyle.errorMessage}>
+									This field is required
+								</span>
+							)}
+							<input
+								className={globalStyle.input}
+								type="password"
+								placeholder="Bekræft nyt password"
+								{...register("confirmNewPassword", {
+									required: true,
+									validate: (value) =>
+										value === watch("newPassword") || "Passwords do not match",
+								})}
+							/>
+							{errors.confirmNewPassword && (
+								<span className={globalStyle.errorMessage}>
+									{errors.confirmNewPassword.message}
+								</span>
+							)}
+							{errorMessage && (
+								<span className={globalStyle.errorMessage}>{errorMessage}</span>
+							)}
+							{successMessage && (
+								<span className={globalStyle.successMessage}>
+									{successMessage}
+								</span>
+							)}
+							<div className={style.buttonContainer}>
+								<button className={globalStyle.styledButton} type="submit">
+									Skift Password
+								</button>
+							</div>
+						</form>
+					</div>
 				</PageWrapper>
 			)}
 		</>
 	);
 };
 // {/* <p>Du er logget ind som {loginData.user.email}</p>
-// 				<div className={style.buttonContainer}>
-// 					{/* Denne knap vil logge brugeren ud */}
-// 					<button onClick={handleLogout} className={globalStyle.inputBtn}>
-// 						Log ud
-// 					</button>
-// 				</div>
-// 				<div className={style.passwordWrapper}>
-// 					<h3 className={globalStyle.subtitle2} style={{ padding: "0" }}>
-// 						Skift Password
-// 					</h3>
-// 					{/* Når der trykkes på knappen (password) kaldes handleChangePassword funktionen */}
-// 					<form
-// 						className={style.form}
-// 						onSubmit={handleSubmit(handleChangePassword)}>
-// 						<input
-// 							className={globalStyle.input}
-// 							type="password"
-// 							placeholder="Nyt password"
-// 							{...register("newPassword", { required: true })}
-// 						/>
-// 						{errors.newPassword && (
-// 							<span className={globalStyle.errorMessage}>
-// 								This field is required
-// 							</span>
-// 						)}
-// 						<input
-// 							className={globalStyle.input}
-// 							type="password"
-// 							placeholder="Bekræft nyt password"
-// 							{...register("confirmNewPassword", {
-// 								required: true,
-// 								validate: (value) =>
-// 									value === watch("newPassword") || "Passwords do not match",
-// 							})}
-// 						/>
-// 						{errors.confirmNewPassword && (
-// 							<span className={globalStyle.errorMessage}>
-// 								{errors.confirmNewPassword.message}
-// 							</span>
-// 						)}
-// 						{errorMessage && (
-// 							<span className={style.errorMessage}>{errorMessage}</span>
-// 						)}
-// 						{successMessage && (
-// 							<span className={style.successMessage}>{successMessage}</span>
-// 						)}
-// 						<div className={style.buttonContainer}>
-// 							<button className={globalStyle.inputBtn} type="submit">
-// 								Skift Password
-// 							</button>
-// 						</div>
-// 					</form>
-// 				</div> */}
+//
+//
